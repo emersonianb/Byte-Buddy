711f70e7458be639e6d4a212d32a2d080fa71bf3
Emerson Ian <90874749+emersonianb@users.noreply.github.com>
2023-06-05 17:05:14 -0300

    Update barra_de_pesquisa.js

--- home/barra_de_pesquisa.js
+++ home/barra_de_pesquisa.js
@@ -1,6 +1,6 @@
 const perguntaForm = document.getElementById("formulario_pergunta");
 
-const openaiKey = "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";
+const openaiKey = "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";
 const consulta_pt1 = "Responda somente com sim ou não se o texto abaixo é relacionado diretamente ou indiretamente a programação:";
 
 var input = document.getElementById("campo_pergunta");
